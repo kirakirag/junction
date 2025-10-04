bbc87d22cafd468b4bfac8e5d6d9981429435caf
Bas Mens <B.Mens@student.tudelft.nl>
2025-10-04 20:14:00 +0200

    Co-authored-by: ondamlco <ondamlco@users.noreply.github.com>

--- server.py
+++ server.py
@@ -1,19 +1,22 @@
 from flask import Flask, request, jsonify
+import db
 
 app = Flask(__name__)
+
+db.ensureDbExists()
+db.loadData()
+conn = db.getConnection()
 
 # GET endpoint
 @app.route("/hello", methods=["GET"])
 def hello():
-    return jsonify({"message": "Hello from your local API!"})
+    return jsonify({"message": db.getData(conn, "2023-01-01", 1)})
 
 # POST endpoint
-@app.route("/echo", methods=["POST"])
-def echo():
+@app.route("/getData", methods=["POST"])
+def getData():
     data = request.get_json(silent=True)
-    if data is None:
-        return jsonify({"error": "Expecting JSON body"}), 400
-    return jsonify({"you_sent": data})
+    return jsonify({"count": db.getData(conn, data["date"], data["city_id"])})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
